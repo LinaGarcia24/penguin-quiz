879da4d7afc0250f0476e160e42a373996dc791c
LinaGarcia24 <linavgarcia24@gmail.com>
2022-02-06 04:24:32 +0000

    Initial JavaScript Structure

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+// Ensure DOM is loaded before anything else is executed
+document.addEventListener('DOMContentLoaded', function() {
+    let question = document.getElementById('question').innerHTML;
+    console.log(question);
+}
+
+function displayQuestion();
+
+function checkAnswer();
+
+function keepCorrectScore();
