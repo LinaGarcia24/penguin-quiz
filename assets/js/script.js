b47650d4b151dbe159891b3ebc4f6def048b87f0
LinaGarcia24 <linavgarcia24@gmail.com>
2022-02-10 02:01:54 +0000

    Add display and check question logic

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", function () {
 });
 
 // variables for questions loop
+let currentQuestionNumber;
 let newQuestion = document.getElementById('question');
 let answerOne = document.getElementById('answer1');
 let answerOneLabel = document.getElementsByTagName('label')[4];
@@ -23,7 +24,7 @@ let questions = [
         c: "Trochilidae",
         d: "Spheniscidae"
     },
-    correctAnswer: "d"
+    correctAnswer: "Spheniscidae"
     },
     { question: "How many species of penguins are there?",
     answers: {
@@ -32,7 +33,7 @@ let questions = [
         c: "20",
         d: "12"
     },
-    correctAnswer: "b"
+    correctAnswer: "18"
     },
     { question: "What species of penguin is the largest?",
     answers: {
@@ -41,7 +42,7 @@ let questions = [
         c: "Emperor",
         d: "Royal"
     },
-    correctAnswer: "c"
+    correctAnswer: "Emperor"
     },
     { question: "What species of penguin is the smallest?",
     answers: {
@@ -50,7 +51,7 @@ let questions = [
         c: "African",
         d: "Humboldt"
     },
-    correctAnswer: "a"
+    correctAnswer: "Fairy"
     },
     { question: "Where do penguins live?",
     answers: {
@@ -59,7 +60,7 @@ let questions = [
         c: "South of the equator",
         d: "North of the equator"
     },
-    correctAnswer: "c"
+    correctAnswer: "South of the equator"
     },
     { question: "How many species of penguins are endangered?",
     answers: {
@@ -68,7 +69,7 @@ let questions = [
         c: "14",
         d: "20"
     },
-    correctAnswer: "a"
+    correctAnswer: "10"
     },
     { question: "What do penguins eat?",
     answers: {
@@ -77,7 +78,7 @@ let questions = [
         c: "krill",
         d: "All of the above"
     },
-    correctAnswer: "d"
+    correctAnswer: "All of the above"
     },
     { question: "How long can Emperor penguins hold their breath underwater?",
     answers: {
@@ -86,7 +87,7 @@ let questions = [
         c: "15 minutes",
         d: "6 minutes"
     },
-    correctAnswer: "b"
+    correctAnswer: "27 minutes"
     },
     { question: "How deep can Emperor penguins swim?",
     answers: {
@@ -95,7 +96,7 @@ let questions = [
         c: "500m",
         d: "300m"
     },
-    correctAnswer: "c"
+    correctAnswer: "500m"
     },
     { question: "What is the longest lifespan of a penguin?",
     answers: {
@@ -104,7 +105,7 @@ let questions = [
         c: "20 years",
         d: "30 years"
     },
-    correctAnswer: "d"
+    correctAnswer: "30 years"
     },
     { question: "Are male penguins the only ones that incubate the eggs?",
     answers: {
@@ -113,7 +114,7 @@ let questions = [
         c: "No, the male and the female take turns",
         d: "Penguins don't incubate their eggs"
     },
-    correctAnswer: "c"
+    correctAnswer: "No, the male and the female take turns"
     },
     { question: "What are baby penguins called?",
     answers: {
@@ -122,26 +123,65 @@ let questions = [
         c: "Both a and b",
         d: "None of the above"
     },
-    correctAnswer: "c"
+    correctAnswer: "Both a and b"
     },
 ]
+
+// variables for answer submit and check
+let selectedAnswer;
+let radioButtons = document.querySelectorAll('input[name="answer"]');
+let submitButton = document.getElementById('submit-btn');
+
+
 /**
  * Displays quiz questions and answers
  */
 function displayQuestion() {
     console.log('form submitted');
     for (let i = 0; i < questions.length; i++){
-        newQuestion.innerText = questions[i].question;
-        answerOne.value = questions[i].answers.a;
+        console.log(questions[i]);
+        currentQuestionNumber = i;
+        console.log(currentQuestionNumber);
+        newQuestion.innerText = questions[currentQuestionNumber].question;
+        answerOne.value = questions[currentQuestionNumber].answers.a;
         answerOneLabel.innerText = answerOne.value;
-        answerTwo.value = questions[i].answers.b;
+        answerTwo.value = questions[currentQuestionNumber].answers.b;
         answerTwoLabel.innerText = answerTwo.value;
-        answerThree.value = questions[i].answers.c;
+        answerThree.value = questions[currentQuestionNumber].answers.c;
         answerThreeLabel.innerText = answerThree.value;
-        answerFour.value = questions[i].answers.d;
+        answerFour.value = questions[currentQuestionNumber].answers.d;
         answerFourLabel.innerText = answerFour.value;
+        submitButton.addEventListener('click', storeAnswer);
+    } 
+}     
+
+/**
+ * Stores answer from radio button
+ */
+
+function storeAnswer() {
+    for (let radioButton of radioButtons) {
+        if (radioButton.checked) {
+            selectedAnswer = radioButton.value;
+            console.log(selectedAnswer);
+            checkAnswer();
+        }
     }
+}
+
+/**
+ * Checks if entered answer matches correct answer
+ * for current question
+ */
+
+function checkAnswer() {
+    console.log('answer has been submitted')
+    if (selectedAnswer === questions[currentQuestionNumber].correctAnswer){
+        alert('Good job! You got it right!')
+
+    } else {
+        alert(`You answered ${selectedAnswer}. The correct answer was ${questions[currentQuestionNumber].correctAnswer}`)
     }
 
-    function submitAnswer() {}
+}
 
